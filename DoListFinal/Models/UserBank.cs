687e73eec7b1babaabf9d70262b9eb688a1eaad2
cornu-ammonis <andrewjones232@gmail.com>
2016-07-01 17:28:01 -0400

    Revert "some changes"
    This reverts commit a7465d2a9dc0a4006b07a00babde4d3155adb39a.

--- DoListFinal/Models/UserBank.cs
+++ DoListFinal/Models/UserBank.cs
@@ -10,6 +10,9 @@ namespace DoListFinal.Models
 
         public int ID { get; set; }
         public string AppUserID { get; set; }
+        //public Dictionary<string, List<List_Items>> user_dict = new Dictionary<string, List<List_Items>>();
+        //public virtual List<List_Items> udict {get; set;} 
+
         public string Item_indexes { get; set; }
     }
 }
